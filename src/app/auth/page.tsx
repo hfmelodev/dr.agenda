a76a0dbc56a27abdc91896d32a74c1fde9d7b37d
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2025-05-27 22:26:43 -0300

    chore: add better auth setup

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { LogIn, UserPlus } from 'lucide-react'
 import Image from 'next/image'
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { relations } from 'drizzle-orm'
 import {
+  boolean,
   integer,
   pgEnum,
   pgTable,
@@ -10,7 +11,63 @@ import {
 } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
-  id: uuid('id').defaultRandom().primaryKey().notNull(),
+  id: text('id').primaryKey().notNull(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
+  emailVerified: boolean('email_verified')
+    .$defaultFn(() => false)
+    .notNull(),
+  image: text('image'),
+  createdAt: timestamp('created_at')
+    .$defaultFn(() => new Date())
+    .notNull(),
+  updatedAt: timestamp('updated_at')
+    .$defaultFn(() => new Date())
+    .notNull(),
+})
+
+export const sessionsTable = pgTable('sessions', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  expiresAt: timestamp('expires_at').notNull(),
+  token: text('token').notNull().unique(),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
+  ipAddress: text('ip_address'),
+  userAgent: text('user_agent'),
+  userId: text('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+})
+
+export const accountsTable = pgTable('accounts', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  accountId: text('account_id').notNull(),
+  providerId: text('provider_id').notNull(),
+  userId: text('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  accessToken: text('access_token'),
+  refreshToken: text('refresh_token'),
+  idToken: text('id_token'),
+  accessTokenExpiresAt: timestamp('access_token_expires_at'),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at'),
+  scope: text('scope'),
+  password: text('password'),
+  createdAt: timestamp('created_at').notNull(),
+  updatedAt: timestamp('updated_at').notNull(),
+})
+
+export const verificationsTable = pgTable('verifications', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  identifier: text('identifier').notNull(),
+  value: text('value').notNull(),
+  expiresAt: timestamp('expires_at').notNull(),
+  createdAt: timestamp('created_at').$defaultFn(
+    () => /* @__PURE__ */ new Date()
+  ),
+  updatedAt: timestamp('updated_at').$defaultFn(
+    () => /* @__PURE__ */ new Date()
+  ),
 })
 
 // Um user possui a varias clínicas
@@ -20,7 +77,7 @@ export const usersTableRelations = relations(usersTable, ({ many }) => ({
 
 // Um user possui a várias clínicas e uma clínica possui muitos users
 export const usersToClinicsTable = pgTable('users_to_clinics', {
-  userId: uuid('user_id')
+  userId: text('user_id')
     .notNull()
     .references(() => usersTable.id, {
       onDelete: 'cascade',
